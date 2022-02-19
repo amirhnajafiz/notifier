26a1737bfd82ff8cffb1582e85183f4fae2b8080
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-19 11:26:58 +0330

    mqtt methods added

--- internal/broker/methods.go
+++ internal/broker/methods.go
@@ -1,1 +1,24 @@
 package broker
+
+import (
+	"fmt"
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func (m MQTT) messagePubHandler() mqtt.MessageHandler {
+	return func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+	}
+}
+
+func (m MQTT) connectHandler() mqtt.OnConnectHandler {
+	return func(client mqtt.Client) {
+		fmt.Println("Connected")
+	}
+}
+
+func (m MQTT) connectLostHandler() mqtt.ConnectionLostHandler {
+	return func(client mqtt.Client, err error) {
+		fmt.Printf("Connect lost: %v", err)
+	}
+}
--- internal/broker/mqtt.go
+++ internal/broker/mqtt.go
@@ -10,24 +10,6 @@ type MQTT struct {
 	Cfg Config
 }
 
-func (m MQTT) messagePubHandler() mqtt.MessageHandler {
-	return func(client mqtt.Client, msg mqtt.Message) {
-		fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
-	}
-}
-
-func (m MQTT) connectHandler() mqtt.OnConnectHandler {
-	return func(client mqtt.Client) {
-		fmt.Println("Connected")
-	}
-}
-
-func (m MQTT) connectLostHandler() mqtt.ConnectionLostHandler {
-	return func(client mqtt.Client, err error) {
-		fmt.Printf("Connect lost: %v", err)
-	}
-}
-
 func (m MQTT) Register(cfg Config) {
 	broker := cfg.Host
 	port := cfg.Port
