b7dc5bc22b3c6f88862333b71d42a0f342f1d6ce
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-19 11:03:21 +0330

    first config method of New added

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,29 @@
 package config
+
+import (
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"log"
+)
+
+type Config struct {
+	Test string `koanf:"test"`
+}
+
+func New() Config {
+	var instance Config
+
+	k := koanf.New(".")
+
+	// Load configuration
+	if err := k.Load(file.Provider("config.yml"), yaml.Parser()); err != nil {
+		log.Printf("error loading config.yml: %s\n", err)
+	}
+
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %s\n", err)
+	}
+
+	return instance
+}
