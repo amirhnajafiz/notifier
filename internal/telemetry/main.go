60f1df33e67836ccfa3b0ea15ecb2957b16402de
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-20 18:21:09 +0330

    telemetry new exporter

--- internal/telemetry/main.go
+++ internal/telemetry/main.go
@@ -1,7 +1,15 @@
 package telemetry
 
-import "io"
+import (
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/trace"
+	"io"
+)
 
 func NewExporter(w io.Writer) (trace.SpanExporter, error) {
-	return stdouttrace
+	return stdouttrace.New(
+		stdouttrace.WithWriter(w),
+		stdouttrace.WithPrettyPrint(),
+		stdouttrace.WithoutTimestamps(),
+	)
 }
