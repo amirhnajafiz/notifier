fae9b9675690343d507bf9251bf29fa01104f60c
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-20 10:13:05 +0330

    handler type init

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -1,11 +1,4 @@
 package handler
 
-import "github.com/gofiber/fiber/v2"
-
-func Publish(c *fiber.Ctx) error {
-	return c.SendString("message published")
-}
-
-func Subscribe(c *fiber.Ctx) error {
-	return c.SendString("message received")
+type Handler struct {
 }
