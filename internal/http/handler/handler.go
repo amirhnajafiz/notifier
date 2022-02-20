fe03aad2a9a8b6f8442e3af46013fa805acd89d0
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-20 10:15:56 +0330

    handler registered

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -1,4 +1,17 @@
 package handler
 
+import (
+	"cmd/internal/client"
+	"github.com/gofiber/fiber/v2"
+)
+
 type Handler struct {
+	Client client.Client
+}
+
+func (h Handler) Register(app *fiber.App) Handler {
+	app.Post("/api/send", h.Publish)
+	app.Get("api//catch", h.Subscribe)
+
+	return h
 }
--- internal/http/handler/methods.go
+++ internal/http/handler/methods.go
@@ -2,10 +2,10 @@ package handler
 
 import "github.com/gofiber/fiber/v2"
 
-func Publish(c *fiber.Ctx) error {
+func (h Handler) Publish(c *fiber.Ctx) error {
 	return c.SendString("message published")
 }
 
-func Subscribe(c *fiber.Ctx) error {
+func (h Handler) Subscribe(c *fiber.Ctx) error {
 	return c.SendString("message received")
 }
