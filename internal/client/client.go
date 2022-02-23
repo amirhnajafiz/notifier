836bc1fc13a67eb2a4f037414408dc78b2e91891
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-23 10:40:43 +0330

    message handler added

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,6 +1,8 @@
 package client
 
 import (
+	"time"
+
 	"cmd/internal/broker"
 	"cmd/internal/cache"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -16,4 +18,14 @@ func (c Client) Register(opts *mqtt.ClientOptions) Client {
 	c.Connection = mqtt.NewClient(opts)
 
 	return c
+}
+
+func (c *Client) MessageHandler() mqtt.MessageHandler {
+	return func(client mqtt.Client, message mqtt.Message) {
+		c.Cache.Put(cache.Message{
+			Topic:   message.Topic(),
+			Content: string(message.Payload()),
+			Date:    time.Now(),
+		})
+	}
 }
