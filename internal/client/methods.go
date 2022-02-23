22b3ce4cca7a255efc82e3ea9ce5324ab48e5d11
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-23 10:07:00 +0330

    client publish and subscribe methods reset

--- internal/client/methods.go
+++ internal/client/methods.go
@@ -1,21 +1,19 @@
 package client
 
-import "time"
+import "fmt"
 
 func (c Client) Publish(topic string, msg string) error {
-	ticker := time.NewTimer(time.Second * 5)
-	for range ticker.C {
-		if token := c.Connection.Publish(topic, 0, false, msg); token.Wait() && token.Error() != nil {
-			return token.Error()
-		}
+	if token := c.Connection.Publish(topic, 0, false, msg); token.Wait() && token.Error() != nil {
+		return fmt.Errorf("publish error: %w", token.Error())
 	}
+
 	return nil
 }
 
-func (c Client) Sub(topic string) (string, string, error) {
+func (c Client) Sub(topic string) error {
 	if token := c.Connection.Subscribe(topic, 1, nil); token.Wait() && token.Error() != nil {
-		return "", "", token.Error()
+		return fmt.Errorf("subscribe failed %w", token.Error())
 	}
 
-	return c.Broker.Topic, c.Broker.Msg, nil
+	return nil
 }
