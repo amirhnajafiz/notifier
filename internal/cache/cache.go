2e6910ea5c569bd38509c4cc10623f8455f501bb
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-26 11:43:55 +0330

    cache config added

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -2,10 +2,10 @@ package cache
 
 import "github.com/go-redis/redis/v8"
 
-func New() *redis.Client {
+func New(cfg Config) *redis.Client {
 	return redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr:     cfg.Host,     // "localhost:6379"
+		Password: cfg.Password, // no password set
+		DB:       0,            // use default DB
 	})
 }
---
+++ internal/cache/config.go
@@ -0,0 +1,6 @@
+package cache
+
+type Config struct {
+	Host     string `json:"host" xml:"host"`
+	Password string `json:"password" xml:"password"`
+}
