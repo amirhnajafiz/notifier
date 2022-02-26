4d707625c756d4bc0f3b715d80a0532bb672b7ed
amirhnajafiz <najafizadeh21@gmail.com>
2022-02-26 12:01:12 +0330

    nickname routes added

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -11,6 +11,9 @@ type Handler struct {
 
 func (h Handler) Register(app *fiber.App) Handler {
 	app.Post("/api/send", h.Publish)
+	app.Put("/api/name", h.SetName)
+	app.Get("/api/name", h.WhoAmI)
+	app.Delete("/api/name", h.RemoveName)
 
 	return h
 }
