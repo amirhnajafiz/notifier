63ecb2441184069ee9f8dc2bf60560999c66f17e
amirhnajafiz <najafizadeh21@gmail.com>
2022-08-27 14:46:04 +0430

    refactor: handler methods

--- internal/http/handler/handler.go
+++ internal/http/handler/handler.go
@@ -11,9 +11,6 @@ type Handler struct {
 
 func (h Handler) Register(app *fiber.App) Handler {
 	app.Post("/api/send", h.Publish)
-	app.Put("/api/name", h.SetName)
-	app.Get("/api/name", h.WhoAmI)
-	app.Delete("/api/name", h.RemoveName)
 
 	return h
 }
--- internal/http/handler/methods.go
+++ internal/http/handler/methods.go
@@ -1,9 +1,7 @@
 package handler
 
 import (
-	"context"
 	"encoding/json"
-	"os"
 	"time"
 
 	"github.com/amirhnajafiz/notifier/internal/http/request"
@@ -12,29 +10,19 @@ import (
 
 func (h Handler) Publish(c *fiber.Ctx) error {
 	var req request.Request
-	_ = c.BodyParser(&req)
 
-	ctx := context.Background()
-	req.ID, _ = os.Hostname()
-	req.Date = time.Now().Format(time.RFC1123)
-
-	val, e := h.Client.Rdb.Get(ctx, req.ID).Result()
-	if e == nil {
-		req.ID = val
+	if err := c.BodyParser(&req); err != nil {
+		return c.SendString(err.Error())
 	}
+
+	req.Date = time.Now().Format(time.RFC1123)
 
 	data, er := json.Marshal(req)
 	if er != nil {
 		return er
 	}
 
-	var topic string
-	if topic = req.Topic; topic == "" {
-		topic = h.Client.Cfg.Topic
-	}
-
-	err := h.Client.Publish(topic, string(data))
-
+	err := h.Client.Publish(req.Topic, string(data))
 	if err != nil {
 		return err
 	}
@@ -44,48 +32,5 @@ func (h Handler) Publish(c *fiber.Ctx) error {
 		"length":    len(req.Msg),
 		"send from": req.ID,
 		"time":      req.Date,
-	})
-}
-
-func (h Handler) SetName(c *fiber.Ctx) error {
-	ctx := context.Background()
-	value := c.Query("nickname")
-	key, _ := os.Hostname()
-
-	if err := h.Client.Rdb.Set(ctx, key, value, 0).Err(); err != nil {
-		return err
-	}
-
-	return c.JSON(fiber.Map{
-		"status":   "200|OK",
-		"name":     key,
-		"nickname": value,
-	})
-}
-
-func (h Handler) RemoveName(c *fiber.Ctx) error {
-	ctx := context.Background()
-	key, _ := os.Hostname()
-
-	if err := h.Client.Rdb.Del(ctx, key); err != nil {
-		return err.Err()
-	}
-
-	return c.JSON(fiber.Map{
-		"status": "200|OK",
-	})
-}
-
-func (h Handler) WhoAmI(c *fiber.Ctx) error {
-	ctx := context.Background()
-	key, _ := os.Hostname()
-
-	value, err := h.Client.Rdb.Get(ctx, key).Result()
-	if err != nil {
-		return err
-	}
-
-	return c.JSON(fiber.Map{
-		"nickname": value,
 	})
 }
